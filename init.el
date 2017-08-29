f039356fbd79c5fa2d4c6eea67a14fde3326bc64
Noam Lewis <noam.lewis@elastifile.com>
2017-08-29 11:38:07 +0300

    wip

---
+++ init.el
@@ -0,0 +1,22 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (noam)))
+ '(custom-safe-themes
+   (quote
+    ("4fbb52182a1af390d59b23669b5397df29c29a8cdd02cd6ec5fc859143c47001" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
